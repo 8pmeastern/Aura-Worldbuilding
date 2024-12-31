446af75e1ecfef0488d1a158046cfa87eafc2b8a
Gabe <8pmeastern@gmail.com>
2024-12-31 18:30:53 -0500

    Quartz sync: Dec 31, 2024, 6:30 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Welcome to Aura",
+    pageTitle: "🌐 Building Aura",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
